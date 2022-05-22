077d8b73ab6ee52ca50d37e2ee5643ce4aeb4d93
n8hjp <63569764+n8hjp@users.noreply.github.com>
2022-05-22 12:42:17 -0400

    Update switch.py
    Copied new code from debecknel/ecoplug

--- custom_components/ecoplug/switch.py
+++ custom_components/ecoplug/switch.py
@@ -1,7 +1,7 @@
 import logging
 
-from homeassistant.const import (DEVICE_DEFAULT_NAME, ATTR_HIDDEN, EVENT_HOMEASSISTANT_STOP)
-from homeassistant.components.switch import SwitchDevice
+from homeassistant.const import (DEVICE_DEFAULT_NAME, ATTR_HIDDEN, EVENT_TIME_CHANGED, EVENT_HOMEASSISTANT_STOP)
+from homeassistant.components.switch import SwitchEntity
 from homeassistant.helpers.entity import ToggleEntity
 from homeassistant.helpers.event import track_time_change
 
@@ -13,10 +13,11 @@ _LOGGER = logging.getLogger(__name__)
 SCAN_INTERVAL = 5
 
 
-class EcoPlugSwitch(SwitchDevice):
+class EcoPlugSwitch(SwitchEntity):
     def __init__(self, plug):
         self._plug = plug
         self._name = plug.name
+        self._unique_id = "switch." + self._plug.plug_data[2].decode('utf-8')
         self._state = self._plug.is_on()
 
     @property
@@ -29,7 +30,29 @@ class EcoPlugSwitch(SwitchDevice):
 
     @property
     def is_on(self):
+        self.update()
         return self._state
+        
+    @property
+    def state(self):
+        if self._state:
+            return "on"
+        return "off"
+        
+    @property
+    def unique_id(self):
+        """Return a unique ID."""
+        return self._unique_id
+    
+    @property
+    def device_state_attributes(self):
+        """Return the device state attributes."""
+        attrs = {
+            "brand": "Eco Plug",
+            "friendly_name": self._name,
+        }
+
+        return attrs
 
     def turn_on(self):
         self._plug.turn_on()
@@ -45,13 +68,13 @@ class EcoPlugSwitch(SwitchDevice):
 
     
 
-def setup_platform(hass, config, add_devices, discovery_info=None):
+def setup_platform(hass, config, add_entities, discovery_info=None):
     from pyecoplug import EcoDiscovery
 
     discovered = {}
     def add(plug):
         if not plug.name in discovered:
-            add_devices([EcoPlugSwitch(plug)])
+            add_entities([EcoPlugSwitch(plug)], True)
             discovered[plug.name] = plug
 
     def remove(plug):
@@ -65,4 +88,3 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
         disco.stop()
 
     hass.bus.listen_once(EVENT_HOMEASSISTANT_STOP, stop_disco)
-
